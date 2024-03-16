e7a6f454094912c7b0a715d7143adc01414fd74b
monster <monster@DESKTOP-JK4TV70>
2024-03-17 01:57:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+using Business.Concrete;
+using Entities.Concrete;
+
+namespace MaskTracking
+{ class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person();
+
+            person.FirstName = "A";
+            person.LastName = "B";
+            person.NationalId = 123;
+            person.BirthYear = 1998;
+
+
+
+            PttManager pttManager = new PttManager(new PersonMenager());
+            pttManager.GiveMask(person);
+        }
+
+  
+
+    }
+
+    class People
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BirthYear { get; set; }
+        public long NationalId { get; set; }
+
+    }
+}
